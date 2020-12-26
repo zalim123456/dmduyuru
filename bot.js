612f70470217c4acd48727edd1178cfafbd5e6c5
zalim123456 <59143212+zalim123456@users.noreply.github.com>
2020-12-26 16:24:52 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+    app.get("/",(requesst, response) => {
+      console.log(`pingleme işlemi başarılı başarılıysa bu yazıyı loglarda görürsün`);
+      response.sendStatus(200);
+    });
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const db = require('quick.db')
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const fs = require("fs");
+const moment = require("moment");
+require("./util/eventLoader")(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+  };
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});///discord.gg/qGWXFs8
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
+
+client.login(process.env.token);
+
+//////////////////////////////////////////////
+
+client.on("ready", () => {
+  client.channels.get("792297864415805470").join();
+})
+
+
+///////////////////////////////////////////////////////
+
+client.on("guildBanAdd", async (guild, user) => {
+  if (!db.has(`banlimit_${guild.id}`)) return;
+  let logs = await guild.fetchAuditLogs({type: 'MEMBER_BAN_ADD'});
+  if (logs.entries.first().executor.bot) return;
+  const kisi = logs.entries.first().executor
+  const member = guild.members.get(kisi.id)
+  if (member.hasPermission('ADMINISTRATOR')) return;
+  let banlimit = db.fetch(`banlimit_${guild.id}`)
+  if (isNaN(banlimit)) return;
+  banlimit = banlimit + 1
+  if (!db.has(`bansayi_${member.id}_${guild.id}`)) {
+    if (banlimit == 1) {
+      var array = member.roles.filter(role => role.name !== "@everyone").array()
+      for (const role of array) {
+        member.removeRole(role.id)
+      }
+    }else{
+      db.set(`bansayi_${member.id}_${guild.id}`, 1)
+    }
+  }else{
+    const bansayisi = db.fetch(`bansayi_${member.id}_${guild.id}`) + 1
+    if (bansayisi >= banlimit) {
+      db.delete(`bansayi_${member.id}_${guild.id}`)
+      var array = member.roles.filter(role => role.name !== "@everyone").array()
+      for (const role of array) {
+        member.removeRole(role.id)
+      }
+    }else{
+      db.add(`bansayi_${member.id}_${guild.id}`, 1)
+    }
+  }
+})
+
+
+
